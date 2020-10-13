e9775eb939aaa8dc858e4e47e76dc7ba9181368a
Daniel García <danhiel.garcia98@gmail.com>
2020-10-13 14:26:55 -0600

    Consulta y búsqueda de contratos

--- src/components/contratos/Contratos.js
+++ src/components/contratos/Contratos.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Skeleton, Card, Space, Row, Col } from 'antd';
+import { Avatar, Skeleton, Card, Space, Row, Col, PageHeader, Input } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined, FileTextOutlined } from '@ant-design/icons';
 import app from '../../firebaseConfig';
+
+const { Search } = Input;
 
 const { Meta } = Card;
 
@@ -13,17 +15,26 @@ class Contratos extends Component
         this.ref = app.firestore().collection('contratos');
         this.unsubscribe = null;
         this.state = {
-            loading: false,
+            loading: true,
             contratos: []
         };
     }
 
     obtenerContratos = (querySnapshot) => {
         const contratos = [];
+        const { busqueda } = this.state;
         this.setState({ loading: true })
 
         querySnapshot.forEach( async (doc) => {
             const { cliente, activo, codigo, fecha_inicio, fecha_fin, velocidad } = doc.data();
+
+            if 
+            (
+                busqueda &&
+                cliente.toLowerCase().indexOf(busqueda) === -1 &&
+                codigo.toLowerCase().indexOf(busqueda) === -1
+            ) 
+                return;
 
             contratos.push({
                 key: doc.id,
@@ -43,50 +54,82 @@ class Contratos extends Component
     }
 
     componentDidMount() {
-        this.unsubscribe = this.ref.onSnapshot(this.obtenerContratos);
+        this.ref.onSnapshot(this.obtenerContratos);
     }
 
     componentDidUpdate() {
 
     }
 
+    buscar(valor) {
+        if (valor.toLowerCase() !== this.state.busqueda) {
+            this.setState({ busqueda: valor.toLowerCase() })
+            this.ref
+            .get()
+            .then(querySnapshot => this.obtenerContratos(querySnapshot));
+        }
+    }
+
     render(){
         const { loading } = this.state;
-        console.log(this.state.contratos);
         return (
             <div>
+                
+                <PageHeader
+                    className="site-page-header"
+                    // onBack={() => null}
+                    title="Contratos"
+                    subTitle="Lista de contratos"
+                    extra={
+                        [
+                            <Search
+                                key="1"
+                                placeholder="Buscar"
+                                onSearch={value => this.buscar(value) }
+                                style={{ width: 200 }}
+                            />
+                        ]
+                    }
+                />
                 <Row gutter={16}>
+                    {
+                        loading && 
+                        [1,2,3,4,5,6,7,8].map(n => 
+                            <Col span={6} key={n}>
+                                <Card style={{ marginTop: 16 }} loading={loading} />
+                            </Col>
+                        )
+                    }
+
                     { this.state.contratos.map(contrato =>
                     <Col span={6} key={contrato.key}>
                         <Card
-                            style={{ width: 300, marginTop: 16 }}
+                            style={{ marginTop: 16 }}
                             actions={[
                                 <SettingOutlined key="setting" />,
                                 <EditOutlined key="edit" />,
                                 <EllipsisOutlined key="ellipsis" />,
                             ]}
                         >
-                        <Skeleton loading={loading} avatar active>
                             <Meta
-                            avatar={
-                                <FileTextOutlined style={{ fontSize: '32px'}} />
-                            }
-                            title={
-                                <Space size="middle">
-                                    <strong>{ contrato.codigo }</strong>
-                                    <strong style={{color: 'green'}}>{contrato.velocidad}Mb</strong>
-                                </Space>
-                            }
-                            description={
-                                <div>
-                                    <strong>Cliente:</strong><br /> {contrato.cliente}
-                                    <strong>Fecha inicio:</strong> {contrato.fecha_inicio}<br />
-                                    <strong>Fecha fin:</strong> {contrato.fecha_fin}<br />
-                                </div>
-                            }
+                                avatar={
+                                    <FileTextOutlined style={{ fontSize: '32px'}} />
+                                }
+                                title={
+                                    <Space size="middle">
+                                        <strong>{ contrato.codigo }</strong>
+                                    </Space>
+                                }
+                                description={
+                                    <div>
+                                        <strong>Cliente:</strong> {contrato.cliente} <br />
+                                        <strong>Fecha inicio:</strong> {contrato.fecha_inicio}<br />
+                                        <strong>Fecha fin:</strong> {contrato.fecha_fin}<br />
+                                        <strong>Velocidad: <span color='green'>{contrato.velocidad}Mb</span> </strong>
+                                    </div>
+                                }
                             />
-                        </Skeleton>
-                    </Card>
+                        </Card>
                     </Col>
                     )}
                 </Row>
